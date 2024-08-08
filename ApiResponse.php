a3f49bdafa757fc895fcaf38114baa653e4687e4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-08 16:02:57 +0800

    1.x

---
+++ ApiResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace MRizki28\ApiRespone;
+
+class ApiResponse{
+
+    /**
+     * Format a successful response.
+     *
+     * @param array $data Data to include in the response.
+     * @param string $message Message to include in the response.
+     * @param int $code HTTP status code.
+     * @return string JSON formatted response.
+     */
+
+    public static function success($data = [], $message = 'Success', $code = 200){
+        http_response_code($code);
+        
+        return json_encode([
+            'status' => 'success',
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+}
