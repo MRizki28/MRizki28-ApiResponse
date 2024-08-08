4d693b65822dce15bcbd697e596dff56688ed41d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-08 19:29:15 +0800

    add not found function

--- src/ApiResponse/ApiResponse.php
+++ src/ApiResponse/ApiResponse.php
@@ -19,11 +19,32 @@ class ApiResponse
 
     public static function success($data = [], $message = 'Success', int $code =  200)
     {
-        return new Response(json_encode([
-            'status' => 'success',
-            'message' => $message,
-            'data' => $data
-        ]),
+        return new Response(
+            json_encode([
+                'status' => 'success',
+                'message' => $message,
+                'data' => $data
+            ]),
+            $code,
+            ['Content-Type' => 'application/json']
+        );
+    }
+
+    /**
+     * Format an data not found response.
+     *
+     * @param string $message Message to include in the response.
+     * @param int $code HTTP status code.
+     * @return string JSON formatted response.
+     */
+
+    public static function notFound($message = 'Data not found', int $code = 404)
+    {
+        return new Response(
+            json_encode([
+                'status' => 'Data not found',
+                'message' => $message
+            ]),
             $code,
             ['Content-Type' => 'application/json']
         );
